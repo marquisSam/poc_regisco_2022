c3218c77270d7ff6f960542187afa87b563157f5
Sam <marquis.sam@gmail.com>
2022-10-07 10:50:55 -0400

    initiate angular regisco env

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { RequestsService } from './services/requests.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'test-regisco';
+  constructor(
+    private req : RequestsService
+  ) { }
+
+  ngOnInit(): void {
+      this.req.fetchTask()
+        .subscribe((data) => {
+          console.log(data)
+        });
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PendingTaskComponent } from './components/pending-task/pending-task.component';
+
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PendingTaskComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
