31c110bda6699d60d5d9564f961dfd46aeda54b7
Sam <marquis.sam@gmail.com>
2022-10-11 10:55:20 -0400

    Augmenting task list model

--- src/app/store/store.models.ts
+++ src/app/store/store.models.ts
@@ -3,9 +3,15 @@ export interface TaskModel {
     "name": string,
     "deadline": string,
     "completedAt": string,
+
     "deadlineTimestamp" : number,
     "compAtTimestamp" : number
-
+    "isCompleted" : boolean,
+    "isLate" : boolean,
+    "isYesterday" : boolean,
+    "isToday" : boolean,
+    "isTomorrow" : boolean,
+    "dateLabel" : boolean,
 }
 
 export interface TaskListModel extends Array<TaskModel> {}
